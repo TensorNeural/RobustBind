2f39e7bfca54af925654f3c5e89bcf277d42196b
Magic Coder <magicoder20@gmail.com>
2025-03-04 20:29:56 +0000

    Refactor attack bind to support Places365

---
+++ scripts/zero_shot_imagenet.sh
@@ -0,0 +1,13 @@
+cd ../
+
+# ImageNet-1K
+CUDA_VISIBLE_DEVICES=0 python infer.py \
+  --test_dataset_dir /home/user/datasets/ImageNet-1K \
+  --test_data_path ./datasets/ImageNet-1K/val_data.json \
+  --centre_embeddings_path ./centre_embs/image_in_center_embeddings.pkl \
+  --pretrain_weights ./ckpts/pretrained_weights.pt \
+  --output_dir ./outputs/val_data_zero_shot \
+  --modality image \
+  --val_batch_size 2000 \
+  --num_workers 4 \
+  --seed 1234
