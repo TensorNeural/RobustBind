Fix reduction of attack effect due to normalization bug (#7)
* Output images & metadata

* Generate adversarial dataset for ImageNet

* Fix reduction of attack effect due to normalization bug

* Format code

autoattack/attack_bind.py
@@ -1,10 +1,15 @@
 import os
 import abc
+import json
+import shutil
 from types import SimpleNamespace
 
 import torch
 import torch_scatter
 from torch.utils.data import DataLoader, Subset
+import torchvision.utils as vutils
+
+from concurrent.futures import ThreadPoolExecutor
 
 from autoattack import AutoAttack
 from imagebind.imagebind_model import ModalityType
@@ -13,9 +18,6 @@ from utils.utils import load_centre_embeddings
 
 
 def unnormalize_inplace(x, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
-    """
-    Reverse normalization in-place: (x * std) + mean, clamped to [0,1].
-    """
     mean_t = torch.tensor(mean, device=x.device).view(1, -1, 1, 1)
     std_t = torch.tensor(std, device=x.device).view(1, -1, 1, 1)
     x.mul_(std_t).add_(mean_t).clamp_(0, 1)
@@ -23,13 +25,34 @@ def unnormalize_inplace(x, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
 
 
 def normalize_inplace(x, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
-    """
-    Normalizes in-place: (x - mean) / std.
-    """
     mean_t = torch.tensor(mean, device=x.device).view(1, -1, 1, 1)
     std_t = torch.tensor(std, device=x.device).view(1, -1, 1, 1)
     x.sub_(mean_t).div_(std_t)
     return x
+
+
+def parallel_save_images(adv_examples, eps_dir, batch_idx):
+    """
+    Saves each sample in adv_examples to eps_dir using multiple threads.
+    Filenames will follow:
+        batch{batch_idx}_idx{i}.png
+    This function does NOT return anything or handle metadata.
+    """
+    adv_cpu = adv_examples.cpu()
+
+    def save_image(tensor_img, path):
+        vutils.save_image(tensor_img, path)
+
+    futures = []
+    with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        for i in range(adv_cpu.size(0)):
+            single_adv = adv_cpu[i]
+            img_filename = f"batch{batch_idx}_idx{i}.png"
+            img_save_path = os.path.join(eps_dir, img_filename)
+            futures.append(executor.submit(save_image, single_adv, img_save_path))
+
+    for f in futures:
+        f.result()
 
 
 class Attack(abc.ABC):
@@ -38,7 +61,6 @@ class Attack(abc.ABC):
         dataset,
         dataset_name="custom_dataset",
         centre_embeddings_path="./centre_embs/image_in_center_embeddings.pkl",
-        save_dir="./results",
         batch_size=25,
         max_samples=50000,
         epsilons=[2 / 255, 4 / 255],
@@ -52,7 +74,6 @@ class Attack(abc.ABC):
         self.dataset = dataset
         self.dataset_name = dataset_name
         self.centre_embeddings_path = centre_embeddings_path
-        self.save_dir = save_dir
         self.batch_size = batch_size
         self.max_samples = max_samples
         self.epsilons = epsilons
@@ -68,7 +89,10 @@ class Attack(abc.ABC):
         self.modality = modality
 
     @abc.abstractmethod
-    def get_label_indices(self, centre_labels, device) -> torch.Tensor:
+    def get_indices_from_labels(self, centre_labels, device) -> torch.Tensor:
+        pass
+
+    def get_labels_from_indices(self, indices) -> list:
         pass
 
 
@@ -83,15 +107,23 @@ modality_map = {
 
 
 class AutoAttackRunner:
-    def __init__(self, device=None):
-        # Hard-coded
+    def __init__(
+        self,
+        dataset_adversary_root: str,
+        metadata_output_root: str,
+        metadata_prefix: str,
+        device=None,
+        debug=False,
+    ):
         self.pretrain_weights = "./ckpts/pretrained_weights.pt"
-
-        # Decide device
         self.device = device or (
             torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         )
-        self.model = None  # We'll load it once we know the user-specified modality
+        self.model = None
+        self.dataset_adversary_root = dataset_adversary_root
+        self.metadata_output_root = metadata_output_root
+        self.metadata_prefix = metadata_prefix
+        self.debug = debug
 
     def _init_model(self, attack_modality: str):
         print("Initializing UniBind with weights:", self.pretrain_weights)
@@ -109,27 +141,29 @@ class AutoAttackRunner:
         self.model.eval()
         print("UniBind model ready.")
 
-    def predict(self, x, centre_embeddings, label_indices, attack):
-        modality = modality_map[attack.modality]
+    def predict(self, x, centre_embeddings, center_label_indices, modality):
+        modality = modality_map[modality]
         x_dict = {modality: x}
-
-        visual_embeddings = self.model.encode_vision(x_dict).to(torch.bfloat16)
+        visual_embeddings = self.model.encode_vision(x_dict)
         visual_embeddings = visual_embeddings / visual_embeddings.norm(
             dim=-1, keepdim=True
         )
         similarity = visual_embeddings @ centre_embeddings.t()
-        similarity = similarity.to(torch.bfloat16)
+        expanded_indices = center_label_indices.expand(similarity.shape[0], -1)
         class_raw_scores, _ = torch_scatter.scatter_max(
-            similarity, label_indices.expand(similarity.shape[0], -1), dim=1
+            similarity, expanded_indices, dim=1
         )
-        return class_raw_scores
+        return class_raw_scores, similarity
 
     def run(self, attack: Attack):
         if self.model is None:
             self._init_model(attack.modality)
 
         if attack.max_samples is not None and attack.max_samples < len(attack.dataset):
-            indices = torch.randperm(len(attack.dataset))[: attack.max_samples]
+            if self.debug:
+                indices = torch.arange(attack.max_samples)
+            else:
+                indices = torch.randperm(len(attack.dataset))[: attack.max_samples]
             attack.dataset = Subset(attack.dataset, indices)
 
         loader = DataLoader(
@@ -137,31 +171,51 @@ class AutoAttackRunner:
             batch_size=attack.batch_size,
             num_workers=4,
             pin_memory=True,
-            shuffle=True,
+            shuffle=False,
             prefetch_factor=8,
             persistent_workers=True,
         )
-        os.makedirs(attack.save_dir, exist_ok=True)
+
+        os.remove(attack.log_path) if os.path.exists(attack.log_path) else None
+        shutil.rmtree(self.dataset_adversary_root, ignore_errors=True)
+        os.makedirs(self.dataset_adversary_root, exist_ok=True)
+        os.makedirs(self.metadata_output_root, exist_ok=True)
 
         print("Loading center embeddings from:", attack.centre_embeddings_path)
         centre_embeddings, centre_labels = load_centre_embeddings(
             attack.centre_embeddings_path, self.device
         )
         centre_embeddings = centre_embeddings.to(
-            self.device, dtype=torch.bfloat16, non_blocking=True
+            self.device, dtype=torch.float32, non_blocking=True
         )
         centre_embeddings /= centre_embeddings.norm(dim=-1, keepdim=True)
 
         print("Mapping center labels to dataset IDs...")
-        label_indices = attack.get_label_indices(centre_labels, self.device)
+        center_label_indices = attack.get_indices_from_labels(
+            centre_labels, self.device
+        )
 
-        def local_predict(x):
-            return self.predict(x, centre_embeddings, label_indices, attack)
+        mean_t = torch.tensor(attack.mean, device=self.device).view(1, -1, 1, 1)
+        std_t = torch.tensor(attack.std, device=self.device).view(1, -1, 1, 1)
+
+        def predict_adapter(x):
+            x_norm = (x - mean_t) / std_t
+            return self.predict(
+                x_norm, centre_embeddings, center_label_indices, attack.modality
+            )
 
         for eps in attack.epsilons:
-            print(f"Running AutoAttack for epsilon = {int(eps * 255)}/255")
+            eps_int = int(eps * 255)
+            print(f"Running AutoAttack for epsilon = {eps_int}/255")
+
+            rel_eps_dir = f"eps{eps_int}"
+            abs_eps_dir = os.path.join(self.dataset_adversary_root, rel_eps_dir)
+            os.makedirs(abs_eps_dir, exist_ok=True)
+
+            adv_metadata_eps = []
+
             adversary = AutoAttack(
-                local_predict,
+                predict_adapter,
                 norm=attack.norm,
                 eps=eps,
                 log_path=attack.log_path,
@@ -170,30 +224,68 @@ class AutoAttackRunner:
             )
 
             for batch_idx, (x_test, y_test) in enumerate(loader):
-                torch.cuda.empty_cache()
-                print(f"Processing batch {batch_idx + 1} for epsilon = {int(eps * 255)}/255")
+                print(f"Processing batch {batch_idx + 1} for epsilon = {eps_int}/255")
 
                 x_test = x_test.to(self.device, dtype=torch.float32, non_blocking=True)
                 y_test = y_test.to(self.device, dtype=torch.int64, non_blocking=True)
+                labels = attack.get_labels_from_indices(y_test)
 
                 x_test_unorm = x_test.clone().detach()
                 unnormalize_inplace(x_test_unorm, attack.mean, attack.std)
 
                 with torch.no_grad():
-                    adv_examples = adversary.run_standard_evaluation(
-                        x_test_unorm, y_test, bs=attack.batch_size
+                    adv_examples, adv_y_test, adv_similarity = (
+                        adversary.run_standard_evaluation(
+                            x_test_unorm,
+                            y_test,
+                            centre_embeddings.shape[0],
+                            bs=attack.batch_size,
+                            return_labels=True,
+                        )
+                    )
+                adv_examples_norm = adv_examples.clone().detach()
+                normalize_inplace(adv_examples_norm, attack.mean, attack.std)
+                outpath = os.path.join(abs_eps_dir, f"eps{eps_int}_{batch_idx}.pth")
+                adv_labels = attack.get_labels_from_indices(adv_y_test)
+                if self.debug:
+                    torch.save(
+                        {
+                            "adv_complete": adv_examples_norm,
+                            "adv_x_test": adv_examples,
+                            "adv_y_test": adv_y_test,
+                            "adv_similarity": adv_similarity,
+                            "adv_labels": adv_labels,
+                            "x_test": x_test,
+                            "y_test": y_test,
+                            "labels": labels,
+                        },
+                        outpath,
+                    )
+                else:
+                    torch.save(
+                        {
+                            "adv_complete": adv_examples_norm,
+                            "x_test": x_test,
+                            "adv_labels": adv_labels,
+                            "labels": labels,
+                        },
+                        outpath,
                     )
 
-                normalize_inplace(adv_examples, attack.mean, attack.std)
-                outpath = os.path.join(
-                    attack.save_dir, f"adv_results_eps{int(eps * 255)}_{batch_idx}.pth"
-                )
-                torch.save(
-                    {"adv_complete": adv_examples, "x_test": x_test, "y_test": y_test},
-                    outpath,
-                )
+                parallel_save_images(adv_examples, abs_eps_dir, batch_idx)
 
-        print(
-            "AutoAttack completed for all epsilon values. .pth files are saved in:",
-            attack.save_dir,
-        )
+                for idx_in_batch in range(adv_examples.size(0)):
+                    img_filename = f"batch{batch_idx}_idx{idx_in_batch}.png"
+                    img_save_path = os.path.join(rel_eps_dir, img_filename)
+                    label_str = labels[idx_in_batch]
+                    adv_metadata_eps.append({"data": img_save_path, "label": label_str})
+                torch.cuda.empty_cache()
+
+            meta_filename = f"{self.metadata_prefix}_eps{eps_int}.json"
+            meta_filepath = os.path.join(self.metadata_output_root, meta_filename)
+            with open(meta_filepath, "w") as f:
+                json.dump(adv_metadata_eps, f, indent=2)
+
+            print(f"Metadata for eps={eps_int} saved to {meta_filepath}")
+
+        print("AutoAttack completed for all epsilon values.")

autoattack/checks.py
@@ -21,7 +21,7 @@ def check_randomized(model, x, y, bs=250, n=5, alpha=1e-4, logger=None):
     outputs = []
     with torch.no_grad():
         for _ in range(n):
-            output = model(x)
+            output, _ = model(x)
             corrcl_curr = (output.max(1)[1] == y).sum().item()
             corrcl.append(corrcl_curr)
             outputs.append(output / (L2_norm(output, keepdim=True) + 1e-10))
@@ -43,7 +43,7 @@ def check_randomized(model, x, y, bs=250, n=5, alpha=1e-4, logger=None):
 
 def check_range_output(model, x, alpha=1e-5, logger=None):
     with torch.no_grad():
-        output = model(x)
+        output, _ = model(x)
     fl = [output.max() < 1. + alpha, output.min() >  -alpha,
         ((output.sum(-1) - 1.).abs() < alpha).all()]
     if all(fl):

autoattack/examples/attack_imagenet.py
@@ -3,36 +3,24 @@ import json
 
 import torch
 import torchvision.datasets as datasets
-import torchvision.transforms as transforms
 
 from autoattack.attack_bind import Attack, AutoAttackRunner
+from utils.data_transform import IMAGE_TRANSFORM, IMAGE_MEAN, IMAGE_STD
 
 
 class ImageNetAttack(Attack):
     def __init__(
         self,
         dataset_root,
-        save_dir="./results",
         batch_size=25,
-        max_samples=50000,
-        epsilons=[2 / 255, 4 / 255],
+        max_samples=25,
+        epsilons=[2 / 255],
         norm="Linf",
         version="custom",
         log_root="./logs",
         modality="image",
     ):
-        mean = [0.485, 0.456, 0.406]
-        std = [0.229, 0.224, 0.225]
-        transform = transforms.Compose(
-            [
-                transforms.Resize(256, antialias=True),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=mean, std=std),
-            ]
-        )
-
-        ds = datasets.ImageFolder(root=dataset_root, transform=transform)
+        ds = datasets.ImageFolder(root=dataset_root, transform=IMAGE_TRANSFORM)
         dataset_name = "ImageNet_1K"
         centre_embeddings_path = "./centre_embs/image_in_center_embeddings.pkl"
         self.center_label_to_wordnet_path = (
@@ -43,7 +31,6 @@ class ImageNetAttack(Attack):
             dataset=ds,
             dataset_name=dataset_name,
             centre_embeddings_path=centre_embeddings_path,
-            save_dir=save_dir,
             batch_size=batch_size,
             max_samples=max_samples,
             epsilons=epsilons,
@@ -51,20 +38,23 @@ class ImageNetAttack(Attack):
             version=version,
             log_root=log_root,
             modality=modality,
-            mean=mean,
-            std=std,
+            mean=IMAGE_MEAN,
+            std=IMAGE_STD,
         )
 
         print(f"Loading WordNet mapping from: {self.center_label_to_wordnet_path}")
         with open(self.center_label_to_wordnet_path, "r") as f:
             self.center_label_to_wordnet = json.load(f)
+
+        class_to_index = self._get_class_to_index()
+        self.idx_to_class = {v: k for k, v in class_to_index.items()}
+        self.wordnet_to_center_label = {
+            v: k for k, v in self.center_label_to_wordnet.items()
+        }
         print("Mapping loaded successfully.")
 
-    def get_label_indices(self, centre_labels, device) -> torch.Tensor:
-        if isinstance(self.dataset, torch.utils.data.Subset):
-            class_to_index = self.dataset.dataset.class_to_idx
-        else:
-            class_to_index = self.dataset.class_to_idx
+    def get_indices_from_labels(self, centre_labels, device) -> torch.Tensor:
+        class_to_index = self._get_class_to_index()
 
         mapped_indices = []
         for lbl in centre_labels:
@@ -74,15 +64,42 @@ class ImageNetAttack(Attack):
 
         return torch.tensor(mapped_indices, dtype=torch.int64, device=device)
 
+    def get_labels_from_indices(self, indices) -> list:
+        labels = []
+
+        for idx in indices:
+            wordnet_id = self.idx_to_class[idx.item()]
+            center_label = self.wordnet_to_center_label.get(wordnet_id, "Unknown")
+            labels.append(center_label)
+
+        return labels
+
+    def _get_class_to_index(self):
+        if isinstance(self.dataset, torch.utils.data.Subset):
+            return self.dataset.dataset.class_to_idx
+        else:
+            return self.dataset.class_to_idx
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--dataset_root", type=str, required=True, help="Root path to ImageNet dataset"
+        "--dataset_root",
+        type=str,
+        default="/home/user/datasets/ImageNet-1K/val",
+        help="Root path to ImageNet dataset",
     )
-    parser.add_argument("--batch_size", type=int, default=200)
+    parser.add_argument(
+        "--dataset_adversary_root",
+        type=str,
+        default="/home/user/datasets/ImageNet-1K/val_adv",
+        help="Output directory for ImageNet adversary dataset",
+    )
+    parser.add_argument("--batch_size", type=int, default=160)
     parser.add_argument("--max_samples", type=int, default=50000)
-    parser.add_argument("--epsilons", nargs="+", type=float, default=[2 / 255, 4 / 255])
+    parser.add_argument(
+        "--epsilons", nargs="+", type=float, default=[1 / 255, 2 / 255, 4 / 255]
+    )
     parser.add_argument("--norm", type=str, default="Linf")
     parser.add_argument("--version", type=str, default="custom")
     parser.add_argument(
@@ -98,12 +115,6 @@ def main():
         help="Which modality to use: 'image', 'audio', etc.",
     )
     parser.add_argument(
-        "--save_dir",
-        type=str,
-        default="./results",
-        help="Output directory for .pth results",
-    )
-    parser.add_argument(
         "--centre_embeddings_path",
         type=str,
         default="./centre_embs/image_in_center_embeddings.pkl",
@@ -114,6 +125,18 @@ def main():
         type=str,
         default="./datasets/ImageNet-1K/center_to_wordnet.json",
         help="Path to center-label-to-WordNet mapping",
+    )
+    parser.add_argument(
+        "--metadata_output_root",
+        type=str,
+        default="./datasets/ImageNet-1K/",
+        help="Output directory for metadata",
+    )
+    parser.add_argument(
+        "--metadata_prefix",
+        type=str,
+        default="val_adv",
+        help="Prefix for metadata files",
     )
 
     args = parser.parse_args()
@@ -128,8 +151,6 @@ def main():
         log_root=args.log_root,
         modality=args.modality,
     )
-
-    attack.save_dir = args.save_dir
     attack.centre_embeddings_path = args.centre_embeddings_path
 
     if (
@@ -138,7 +159,11 @@ def main():
     ):
         attack.center_label_to_wordnet_path = args.center_label_to_wordnet_path
 
-    runner = AutoAttackRunner()
+    runner = AutoAttackRunner(
+        dataset_adversary_root=args.dataset_adversary_root,
+        metadata_output_root=args.metadata_output_root,
+        metadata_prefix=args.metadata_prefix,
+    )
     runner.run(attack)
 
 

model.py
@@ -10,7 +10,7 @@ class UniBind(nn.Module):
         super(UniBind, self).__init__()
         self.modality = args.modality
         self.backbone = models.PointBind_I2PMAE()
-        state_dict = torch.load(args.pretrain_weights, map_location='cpu')
+        state_dict = torch.load(args.pretrain_weights, map_location='cpu', weights_only=True)
         self.backbone.load_state_dict(state_dict, strict=True)
         for param in self.backbone.parameters():
             param.requires_grad_(False)